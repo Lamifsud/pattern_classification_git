b442c70d3e11ec5c33867beb85a9e996befb6be5
laurie.m <laurie.mifsud@univ-amu.fr>
2024-04-18 13:27:57 +0200

    function that open a matlab behaviour structure from OrganizeBehaviour and save it in a python dictionary.

---
+++ pipeline_drafts/matlab_file.py
@@ -0,0 +1,45 @@
+import os
+import scipy.io as sio 
+
+def open(path):
+    """
+    Open a matlab behaviour structure from OrganizeBehaviour and save it in a python dictionary. One
+    SESSION corresponds to one behavioural file - independently of the number of probes.
+    :param filename: name of the matlab structure containing the behavioural information
+    :return: behaviour: dictionary containing the same fields as the behaviour structure from matlab
+    """
+    # initialize a counter for the number of files loaded
+    n_file = -1
+    # get filenames in list in the session directory
+    filenames = os.listdir(path)
+
+    data = [] 
+    load_info = []
+    
+    # Loop through MATLAB files in the session directory
+    for matfile in filenames:
+
+        n_file += 1  
+        print(f'loading {matfile}')
+        load_info.append((n_file, matfile))
+
+        # get the full file path for the current file and load MATLAB file
+        filepath = os.path.join(path, matfile) 
+        matfile = sio.loadmat(filepath)
+
+        # get the name of the sub-structure within the MATLAB file
+        data_str_name = list(matfile.keys())[-1]
+
+        # get the field names within the sub-structure and use them as keys for the new dictionary
+        fields = []
+        for key in matfile[data_str_name].dtype.fields.keys():
+            fields.append(key)
+
+        # Extract the data inside the sub-structure and save dictionnary in list
+        data_ = matfile[data_str_name][0][0]
+        data.append({field: data_[i_field][:, 0] for i_field, field in enumerate(fields)})
+        
+
+    print(f'\n{n_file + 1} files loaded')
+    
+    return data, load_info
