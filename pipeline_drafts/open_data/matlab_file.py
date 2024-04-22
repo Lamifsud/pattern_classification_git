9e3fb29d967848dd1f841c2ea71fc377396b9e0b
laurie.m <laurie.mifsud@univ-amu.fr>
2024-04-22 17:53:13 +0200

    update

---
+++ pipeline_drafts/open_data/matlab_file.py
@@ -0,0 +1,204 @@
+import os
+import scipy.io as sio 
+import pandas as pd
+import numpy as np
+
+
+
+def open(path):
+    """
+    Open a matlab behaviour structure from OrganizeBehaviour and save it in a python dictionary. One
+    SESSION corresponds to one behavioural file - independently of the number of probes.
+    :param filename: name of the matlab structure containing the behavioural information
+    :return: behaviour: dictionary containing the same fields as the behaviour structure from matlab
+    """
+    # initialize a counter for the number of files loaded
+    n_file = -1
+    # get filenames in list in the session directory
+    filenames = os.listdir(path)
+
+    data = [] 
+    load_info = []
+    
+    # Loop through MATLAB files in the session directory
+    for matfile in filenames:
+
+        n_file += 1  
+        #print(f'loading {matfile}')
+        load_info.append((n_file, matfile))
+
+        # get the full file path for the current file and load MATLAB file
+        filepath = os.path.join(path, matfile) 
+        matfile = sio.loadmat(filepath)
+
+        # get the name of the sub-structure within the MATLAB file
+        data_str_name = list(matfile.keys())[-1]
+
+        # get the field names within the sub-structure and use them as keys for the new dictionary
+        fields = []
+        for key in matfile[data_str_name].dtype.fields.keys():
+            fields.append(key)
+
+        # Extract the data inside the sub-structure and save dictionnary in list
+        data_ = matfile[data_str_name][0][0]
+        data.append({field: data_[i_field][:, 0] for i_field, field in enumerate(fields)})
+        
+
+    print(f'\n{n_file + 1} files loaded')
+    
+    return data, load_info
+
+
+def clean(info_path):
+    """
+    Clean the data from an Excel file containing information about behavioural data.
+    
+    Parameters:
+    -----------
+    info_path : str
+        Path to the Excel file containing the information.
+
+    Returns:
+    --------
+    df : pandas.DataFrame
+        Cleaned DataFrame containing the behavioural information.
+    """
+    # Read the Excel file into a pandas DataFrame
+    df = pd.read_excel(info_path)
+    
+    # Drop rows where all values are NaN
+    df.dropna(how='all', inplace=True)
+    
+    # Drop unnecessary columns
+    df.drop(columns=['BehDir', 'pitch', 'A/P', 'Lat', 'depth', 'SNR', 'Unnamed: 14', 'depth (no buffer)'], inplace=True)
+    
+    # Rename column
+    df.rename(columns={'%Plexon_spike_file' : 'Plexon_spike_file'}, inplace=True)
+    
+    # Convert float columns to integer
+    float_column_names = df.select_dtypes(include=['float']).columns
+    df[float_column_names] = df[float_column_names].astype(int)
+    
+    return df
+
+def extract_spike_times(load_info, data):
+    """
+    Extract spike times from the given data.
+
+    Parameters:
+    -----------
+    load_info : list of tuples
+        List containing information about loaded files, where each tuple is in the format (n_file, matfile).
+    data : list of dictionaries
+        List containing data dictionaries for each unit.
+
+    Returns:
+    --------
+    spike_times : list of tuples
+        List containing tuples of (loading info, spike times) for each unit.
+    """
+    spike_times = []
+    for unit_idx in range(len(data)):
+        spike_times.append((load_info[unit_idx], data[unit_idx]['ts']))  
+    return spike_times
+
+
+def extract_task_data(load_info, data, target_keys, event_keys_OFF):
+    """
+    Extract task data from the given data.
+
+    Parameters:
+    -----------
+    load_info : list of tuples
+        List containing information about loaded files, where each tuple is in the format (n_file, matfile).
+    data : list of dictionaries
+        List containing data dictionaries for each unit.
+    target_keys : list
+        List of keys for target information.
+    event_keys_OFF : list
+        List of keys for event information to be excluded.
+
+    Returns:
+    --------
+    task_data : list of tuples
+        List containing tuples of (loading info, task data dictionary) for each unit.
+    """
+    task_data = []
+    for unit_idx in range(len(data)):
+        times_keys = [key for key in data[unit_idx] if key != 'ts' and key not in target_keys and key not in event_keys_OFF]
+        task_info = {}
+        for key_time in times_keys:
+            task_info[key_time] = data[unit_idx][key_time]
+        task_data.append((load_info[unit_idx], task_info))
+    return task_data
+
+
+def extract_target_info(load_info, data, target_keys):
+    """
+    Extract target information from the given data.
+
+    Parameters:
+    -----------
+    load_info : list of tuples
+        List containing information about loaded files, where each tuple is in the format (n_file, matfile).
+    data : list of dictionaries
+        List containing data dictionaries for each unit.
+    target_keys : list
+        List of keys for target information.
+
+    Returns:
+    --------
+    target_info : list of tuples
+        List containing tuples of (loading info, target information dictionary) for each unit.
+    """
+    target_info = []
+    for unit_idx in range(len(data)):
+        target_ = {}
+        for key_target in target_keys:
+            target_[key_target] = data[unit_idx][key_target]
+        target_info.append((load_info[unit_idx], target_))
+    return target_info
+
+
+def get_event_labels(task_data):
+    """
+    Get event labels from the given task data.
+
+    Parameters:
+    -----------
+    task_data : list of tuples
+        List containing tuples of (loading info, task data dictionary) for each unit.
+
+    Returns:
+    --------
+    event_labels : list
+        List of event labels extracted from task data.
+    """
+    event_labels = list(task_data[0][1].keys())
+    return event_labels
+
+
+# get event times by trial for each neuron
+def get_event_times(task_data, event_labels):
+    """
+    Retrieves event times from task data file sorts them for each unit
+
+    Args:
+    - task_data (list): List of tuples where each tuple contains file information and a dictionary of task-related data.
+
+    Returns:
+    - event_times (list of numpy.ndarray): List of arrays containing event times.
+
+    Example usage:
+    - event_labels, event_times, event_times_vector = event_times(task_data)
+    """
+
+    # Get event times by event labels
+    event_times = []
+    for unit_idx in range(len(task_data)):
+        times = []
+        for event in event_labels:
+            times.append(task_data[unit_idx][1][event])
+        event_times.append((task_data[unit_idx][0], np.array(times, dtype=object)))
+
+    return event_times
---
+++ pipeline_drafts/structure_data/structure.py
@@ -0,0 +1,187 @@
+import re
+import pandas as pd
+import numpy as np
+from neo.core import Event
+from quantities import s 
+from quantities import millisecond as ms
+
+def info(load_info, df, session):
+    """
+    Extract information about probes, contacts, and units from filenames and match them with corresponding data from an Excel file.
+    
+    Parameters:
+    -----------
+    load_info : list of tuples
+        List containing information about loaded files, where each tuple is in the format (n_file, matfile).
+    df : pandas.DataFrame
+        DataFrame containing information about behavioural data, typically loaded from an Excel file.
+    session : str
+        Identifier for the session.
+
+    Returns:
+    --------
+    info_units : list of dictionaries
+        List of dictionaries containing information about probes, contacts, and units extracted from filenames.
+    info_session : pandas.DataFrame
+        DataFrame containing information about probes, contacts, and units matched with corresponding data from the Excel file.
+    """
+    # Initialize lists to store probe, contact, and unit information
+    list_prob = []
+    list_chan = []
+    list_unit = []
+
+    info_units = []
+
+    # Extract probe, contact, and unit information from filenames and store in dictionaries
+    for i in range(len(load_info)) :
+        matfile = load_info[i][1]
+        match_probe = re.search(r'probe(\d+)', matfile)
+        match_contact = re.search(r'contact(\d+)', matfile)
+        match_unit = re.search(r'unit(\d+)', matfile)
+
+        info_units.append({
+            'probe': int(match_probe.group(1)), 
+            'contact': int(match_contact.group(1)), 
+            'unit': int(match_unit.group(1))
+        })
+
+        list_prob.append(int(match_probe.group(1)))
+        list_chan.append(int(match_contact.group(1)))
+        list_unit.append(int(match_unit.group(1)))
+
+    # Filter data from the Excel file based on session identifier and probe, contact, and unit information
+    sub_info_session = df[df['Plexon_spike_file'].str.startswith(f'{session}')]
+
+    info_session = pd.DataFrame()
+    list_idx = []
+
+    # Match probe, contact, and unit information with corresponding data from the Excel file
+    for i in range(len(list_unit)):
+        sub_data = sub_info_session[(sub_info_session['probe'] == list_prob[i]) & 
+                                    (sub_info_session['channel'] == list_chan[i]) & 
+                                    (sub_info_session['unit'] == list_unit[i])]
+        list_idx.append(sub_data.index[0])
+        info_session = pd.concat((info_session, sub_data))
+
+    return info_units, info_session
+
+
+def elitrials(df):
+    """
+    Convert a string representing Elite trials (in the format '[start:end start:end ...]') into a list of lists.
+    
+    Parameters:
+    -----------
+    entry : str
+        String representing Elite trials.
+
+    Returns:
+    --------
+    result : list of lists or None
+        List of lists containing start and end pairs of Elite trials, or None if the entry is NaN.
+    """
+    if pd.notna(df):  # Check for NaN
+        # Remove square brackets and split by space to separate pairs
+        pairs = df.replace('[', '').replace(']', '').split()
+        result = []
+        for pair in pairs:
+            start, end = map(int, pair.split(':'))
+            result.append([start, end])
+        return result
+    else:
+        return None
+
+
+def CompleteTasktime(info_session, load_info, session):
+    """
+    Process information from info_session DataFrame.
+
+    Parameters:
+    -----------
+    info_session : pandas.DataFrame
+        DataFrame containing information about session data.
+    load_info : list of tuples
+        List containing information about loaded files, where each tuple is in the format (n_file, matfile).
+    session : str
+        Identifier for the session.
+
+    Returns:
+    --------
+    None
+    """
+    # Calculate the first and last bloc start/stop times
+    first_bloc = info_session['start'].min()
+    last_bloc = info_session['stop'].max()
+
+    # Print the start and stop times
+    #print(f'start / stop:\n   {first_bloc} /   {last_bloc}')
+
+    # Filter tasktime data where start and stop times match the first and last blocs and elitrials is NaN
+    tasktime = info_session[(info_session['start'] == first_bloc) & 
+                            (info_session['stop'] == last_bloc) & 
+                            (info_session['elitrials'].isna())]
+
+    # Check if there is any complete tasktime
+    tasktimeComplete = tasktime.shape[0] != 0
+
+    # If there is complete tasktime
+    if tasktimeComplete:
+        # Get unit information
+        unit_info = tasktime[['probe', 'channel', 'unit']].values[0]
+        print(f"{tasktime[['start', 'stop', 'elitrials']]} \n\nunit_info : {unit_info}")
+
+        # Construct matfile name
+        matfile = f'{session}_probe{unit_info[0]}_contact{unit_info[1]}_unit{unit_info[2]}.mat'
+
+        # Find the loading index of the complete unit
+        for i in load_info:
+            if i[1] == matfile:
+                completeUnit = i[0]
+                print(f"{i[1]}\n\nunit's loading index = {completeUnit}")
+
+        return completeUnit
+
+    # If there is no complete tasktime
+    else:
+        print('ANY UNIT RECORDED OVER ENTIRE SESSION') 
+        print(f"check here to construct session times by hand :\n {info_session['start'] == first_bloc}")
+
+
+def events_by_trial(event_times, event_labels):
+    '''
+    Structure periods timestamps by trial and store in a data frame.
+
+    Args:
+    - event_times (numpy.ndarray): Array of event times where rows represent events, and columns represent trials.
+    - event_labels (list): List of event labels corresponding to the columns of the data frame.
+
+    Returns:
+    - trials_ts (list of numpy.ndarray): List of event times structured by trial.
+    - df_task_ts (list of pandas.DataFrame): List of data frames containing event times structured by trial.
+    - events (list of list): List of Event objects.
+
+    '''
+    trials_ts = []  # List to store event times structured by trial
+    df_task_ts_by_neuron = []  # List to store data frames containing event times structured by trial
+    events = []  # List to store Event objects
+
+    for unit_idx in range(len(event_times)):
+        n_events = event_times[unit_idx][1].shape[0]
+        n_trials = event_times[unit_idx][1].shape[1]
+
+        events_ts = np.zeros((n_trials, n_events), dtype=int)
+        event_list = []
+
+        # Split time stamps for each period according to the trial
+        for trial in range(n_trials):
+            for event in range(n_events):
+                events_ts[trial][event] = event_times[unit_idx][1][event][trial]
+            
+            event_list.append(Event(events_ts[trial] * ms, labels=event_labels, dtype='U'))
+
+        events.append((event_times[unit_idx][0], event_list))
+        trials_ts.append((event_times[unit_idx][0], events_ts))
+        task_ts = pd.DataFrame(events_ts, columns=event_labels)
+        df_task_ts_by_neuron.append((event_times[unit_idx][0], task_ts))
+
+    return trials_ts, df_task_ts_by_neuron, events
